fbd24cd05a08f07a8cb170325c11eac489bc684a
Henson <sheng.he.china@gmail.com>
2012-11-15 12:59:42 +0800

    commit

---
+++ clear.bat
@@ -0,0 +1,26 @@
+del /s/q/f .\Release\*.pdb
+del /s/q/f .\sqlite3\Release
+del /s *.obj
+del /s *.pch
+rem del /s *.pdb
+rem del /s *.tli
+rem del /s *.tlh
+del /s *.idb
+del /s *.exp
+del /s *.res
+del /s *.ilk
+del /s *.tmp
+del /s *.ncb
+del /s *.opt
+del /s *.sbr
+del /s *.bak
+del /s *.log
+del /s *.dmp
+del /s *.ncb
+del /s *Log.htm
+del /s *.dep
+del /s *.bsc
+del /s Rca*
+rem del /s .manifest
+rem del /s svn
+del thumbs.db /s/f/q/a
